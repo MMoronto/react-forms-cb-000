08e06226c81ff0f95df80af03881ec4c696cca22
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-06-02 19:56:38 +0000

    Automatically backed up by Learn

--- src/components/DisplayData.js
+++ src/components/DisplayData.js
@@ -5,7 +5,10 @@ class DisplayData extends React.Component {
     return (
       <div>
         <h1>{this.props.formData.firstName}</h1>
+        <h1>{this.props.formData.lastName}</h1>
       </div>
       )
   }
 }
+
+export default DisplayData
