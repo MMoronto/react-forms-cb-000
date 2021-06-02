aef1c7b417d8a0258685606d3c0aa2588e180882
MOBOLA MORONTO <t2wycemo@gmail.com>
2021-06-02 20:04:29 +0000

    Automatically backed up by Learn

--- src/components/ParentComponent.js
+++ src/components/ParentComponent.js
@@ -7,13 +7,13 @@ class ParentComponent extends React.Component {
     firstName: "",
     lastName: "",
   }
-  
+ 
   handleFirstNameChange = event => {
     this.setState({
       firstName: event.target.value
     })
   }
-  
+ 
   handleLastNameChange = event => {
     this.setState({
       lastName: event.target.value
